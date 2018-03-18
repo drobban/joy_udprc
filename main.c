34a99dcdfb8f818dff36657c45a59b3d7c80fb21
David Bern <david.ml.bern@gmail.com>
2018-03-18 13:38:42 +0100

    Starting to read.
    Reading all items every time data is available.
    Printing usage name in readable format.

--- main.c
+++ main.c
@@ -1,8 +1,17 @@
 /* Author: David Bern
  * Date: 2018-03-17
+ * Compile: gcc main.c -o joy_udprc -lusbhid
+ *
+ * todo:
+ * start with parsing arguments. - ok
+ * Open device for reading, make some debugs outputs. - 50% need to read data
+ * Read changes.
+ * Make binding functionality.
+ * Wrap error handling with cleanup routine
  */
 
 #include <sys/types.h>
+#include <sys/select.h>
 
 #include <dev/usb/usb.h>
 #include <dev/usb/usbhid.h>
@@ -19,36 +28,21 @@
 #include <usbhid.h>
 #include <stdarg.h>
 
-#include <sys/select.h>
 
 #define N_JOYSTICKS 1
 
-
 struct Report {
-	struct usb_ctl_report *buffer;
-
-	enum {srs_uninit, srs_clean, srs_dirty} status;
-	int report_id;
-	size_t size;
+  struct usb_ctl_report *buffer;
+  enum {srs_uninit, srs_clean, srs_dirty} status;
+  int report_id;
+  size_t size;
 };
 
-/* todo:
- * start with parsing arguments. - ok
- * Open device for reading, make some debugs outputs. - 50% need to read data
- * Read changes.
- * Make binding functionality.
- */
-
-
-/* Examine this ones use
- * Its an array with struct elements.
- * Used to fetch options to hid_report_size
- * Make this less magical.
- */
+/* make this less magical */
 static struct {
-	int uhid_report;
-	hid_kind_t hid_kind;
-	char const *name;
+  int uhid_report;
+  hid_kind_t hid_kind;
+  char const *name;
 } const reptoparam[] = {
 #define REPORT_INPUT 0
 	{ UHID_INPUT_REPORT, hid_input, "input" },
@@ -59,9 +53,9 @@ static struct {
 #define REPORT_MAXVAL 2
 };
 
-
-
+/* Activates debug printouts */
 unsigned int verbose;
+
 
 static void usage(void)
 {
@@ -103,10 +97,59 @@ static int allocate_report(struct Report *report, size_t size)
   return calculated_size;
 }
 
-static int readloop(int joystick_fd, report_desc_t report_desc, int report_id)
+static int free_report(struct Report *report)
+{
+  free(report->buffer);
+  report->status = srs_uninit;
+}
+
+
+void hid_read(int j_fd, struct Report *j_rep, int r_id, report_desc_t r_desc)
 {
   u_char *message_buffer;
-  size_t message_len, read_len;
+  size_t message_len;
+  int read_len = -1;
+  struct hid_data *hdata;
+  struct hid_item hitem;
+  const char *usage_string;
+
+  message_len = j_rep->size;
+  message_buffer = j_rep->buffer->ucr_data;
+
+  read_len = read(j_fd, message_buffer, message_len);
+
+  /* Instead of dying, return so we can reset ardupilot comms */
+  if (read_len < 0) {
+    err(1, "Error: read from fd %d ", j_fd);
+  }
+  if (read_len != message_len) {
+    errx(1, "Unexpected message len, should be: %d is %d",
+         message_len, read_len);
+  }
+
+  debug_printf("Size of read: %d\n", read_len);
+  debug_printf("Got:\n%s\n", message_buffer);
+  hdata = hid_start_parse(r_desc, 1 << hid_input, r_id);
+  if (hdata == NULL) {
+    errx(1, "Parser failed to start");
+  }
+
+  /* Read until end of items
+   * Should be changed to, only get changed data.
+   * Makes no sense to iterate the entire list.
+   */
+  while (hid_get_item(hdata, &hitem)) {
+    if (hitem.usage) {
+      usage_string = hid_usage_in_page(hitem.usage);
+      debug_printf("Current val: %d\t", hid_get_data(message_buffer, &hitem));
+      debug_printf("Usage: %s\n", usage_string);
+    }
+  }
+  hid_end_parse(hdata);
+}
+
+static int readloop(int joystick_fd, report_desc_t report_desc, int report_id)
+{
   int retval = 0;
   struct timeval timeout;
   fd_set input_fds;
@@ -130,13 +173,9 @@ static int readloop(int joystick_fd, report_desc_t report_desc, int report_id)
       errx(1, "Unable to allocate memory to buffer");
     }
   }
-	message_len = joystick_report.size;
-	message_buffer = joystick_report.buffer->ucr_data;
 
-
+  /* Read joystick / Send ardupilot */
   while (retval != -1) {
-    read_len = -1;
-
     /* set new timeout */
     timeout = (struct timeval){.tv_sec = 2, .tv_usec = 0};
 
@@ -149,8 +188,10 @@ static int readloop(int joystick_fd, report_desc_t report_desc, int report_id)
       if (FD_ISSET(joystick_fd, &input_fds)) {
         debug_printf("Data available on %d\n", joystick_fd);
         /* Read the available data and process it */
-        read_len = read(joystick_fd, message_buffer, message_len);
-        debug_printf("Got:\n%s\n", message_buffer);
+        hid_read(joystick_fd,
+                 &joystick_report,
+                 report_id,
+                 report_desc);
       } else {
         debug_printf("Unexpected, no FD was set\n");
       }
@@ -158,18 +199,18 @@ static int readloop(int joystick_fd, report_desc_t report_desc, int report_id)
       debug_printf("Timeout occured\n");
     }
   }
-
+  free_report(&joystick_report);
   return 0;
 }
 
 int main(int argc, char **argv)
 {
   char const *dev = NULL;
-	char const *table = NULL;
-	char name_buf[PATH_MAX];
+  char const *table = NULL;
+  char name_buf[PATH_MAX];
   int ch;
   int joystick_fd;
-	report_desc_t report_desc;
+  report_desc_t report_desc;
   static int report_id = 0;
 
   verbose = 0;
@@ -194,40 +235,40 @@ int main(int argc, char **argv)
   }
 
   /* Init with default table */
-	if (hid_start(table) == -1) {
-		errx(1, "hid_init");
+  if (hid_start(table) == -1) {
+    errx(1, "hid_init");
   }
 
   /* lets fill in the blanks for the lazy */
 	if (dev[0] != '/') {
-		snprintf(name_buf, sizeof(name_buf), "/dev/%s%s",
+    snprintf(name_buf, sizeof(name_buf), "/dev/%s%s",
              isdigit((unsigned char)dev[0]) ? "uhid" : "", dev);
-		dev = name_buf;
-	}
+    dev = name_buf;
+  }
 
   /* In the future in would be nice to have some sort of force feedback
    * When that happens, we need to open the device for writing as well
    */
-	joystick_fd = open(dev, O_RDONLY);
-	if (joystick_fd < 0) {
-		err(1, "%s", dev);
+  joystick_fd = open(dev, O_RDONLY);
+  if (joystick_fd < 0) {
+    err(1, "%s", dev);
   }
 
 
-	if (ioctl(joystick_fd, USB_GET_REPORT_ID, &report_id) < 0) {
-		report_id = -1;
+  if (ioctl(joystick_fd, USB_GET_REPORT_ID, &report_id) < 0) {
+    report_id = -1;
   }
 
   debug_printf("report ID=%d\n", report_id);
 
 	report_desc = hid_get_report_desc(joystick_fd);
-	if (report_desc == 0) {
-		errx(1, "USB_GET_REPORT_DESC");
+  if (report_desc == 0) {
+    errx(1, "Unable to obtain USB report description");
   }
 
   readloop(joystick_fd, report_desc, report_id);
 
   /* Free report description obtained by hid_get_report_desc() */
-	hid_dispose_report_desc(report_desc);
+  hid_dispose_report_desc(report_desc);
   return 0;
 }
